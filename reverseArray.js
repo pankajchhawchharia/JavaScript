2c827b44d3f4cccf508ae28cd4fe917ae1b9a6f1
Pankaj Chhawchharia <pankaj05011994@gmail.com>
2024-12-08 12:33:42 +0530

    Create reverseArray.js
    reverse an array

---
+++ reverseArray.js
@@ -0,0 +1,7 @@
+// waf to reverse an array
+function reverseArray(arr){
+    console.log("array: ",arr);
+    return arr.reverse();
+}
+console.log("array after reverse: ",reverseArray(["xyz","abc"]));
+console.log("array after reverse: ",reverseArray([1,2,3,4,5]));
