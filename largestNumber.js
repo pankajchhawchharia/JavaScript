135c6e19dee552a2bfd93c92c6b063119b3b2856
Pankaj Chhawchharia <pankaj05011994@gmail.com>
2024-12-09 12:32:39 +0530

    Update largestNumber.js without using Math.max and with Math.max(...arr)
    without using Math.max and with Math.max(...arr)

--- largestNumber.js
+++ largestNumber.js
@@ -4,3 +4,18 @@ function largestNum(arr) {
   return Math.max(...arr);
 }
 console.log("largest number in an array : ",largestNum([32,33,43,445,32,2323,4,3,4]));
+// -----------------or
+
+
+// waf to find the largest number in an array
+function largestNumber(arr) {
+    console.log("array:",arr);
+    let largeNum = arr[0];
+    for (let i = 0;i<arr.length;i++) {
+        if(arr[i] > largeNum)  {
+            largeNum = arr[i];
+        }
+    }
+    return largeNum;
+}
+console.log("largest number in an array:",largestNumber([1,2,3,4,45,5,65,6,3]));
